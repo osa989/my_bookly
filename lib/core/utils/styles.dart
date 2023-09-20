a38dee1d9ea1d7442d06d94c39b97863a1876257
osa989 <osamagamal>
2023-09-20 04:01:29 +0300

    styles to the text part 1

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
+  static const textStyle18 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 }
