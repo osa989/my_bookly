0aef8b06747af3b06461b17369af4f36c69e1386
osa989 <osamagamal>
2023-09-25 14:35:30 +0300

    implement fetch newset books

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -2,13 +2,28 @@ import 'package:dartz/dartz.dart';
 import 'package:my_bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:my_bookly/Features/home/data/repos/home_repo.dart';
 import 'package:my_bookly/core/errors/failures.dart';
+import 'package:my_bookly/core/utils/api_service.dart';
 
-class HomeRepoImpl implements HomeRepo
-{
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              "volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming");
+
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
@@ -16,5 +31,4 @@ class HomeRepoImpl implements HomeRepo
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
-  
 }
--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,13 +1,13 @@
 import 'package:dio/dio.dart';
 
 class ApiService {
-  final baseUrl = "https://www.googleapis.com/books/v1/volumes";
-  final Dio dio;
+  final _baseUrl = "https://www.googleapis.com/books/v1/volumes";
+  final Dio _dio;
   ApiService(
-    this.dio,
+    this._dio,
   );
   Future<Map<String, dynamic>> get({required String endPoint}) async {
-    var response = await dio.get("$baseUrl$endPoint");
+    var response = await _dio.get("$_baseUrl$endPoint");
     return response.data;
   }
 }
